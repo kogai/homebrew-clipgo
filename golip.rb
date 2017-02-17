cbfc09851d8e96442d91769fed2cb63738d0ce48
小飼慎一 <kogaishinichi@kogaishinichinoMacBook-Pro.local>
2017-02-17 15:01:04 +0900

    update sha

--- golip.rb
+++ golip.rb
@@ -5,11 +5,11 @@ TOOL_VERSION = '0.1.3'
 class Golip < Formula
   if Hardware.is_64_bit?
     url "https://github.com/kogai/golip/releases/download/v#{TOOL_VERSION}/golip_#{TOOL_VERSION}_darwin_amd64.zip"
-    sha1 "0d22f77af72d40b1c79f53fae84df7bbae4f73c2"
+    sha256 "d0816d41357b9b47d165d3e97b2ccdb749ad6310c3179eb0ea6a123bb95a6b27"
     version "v#{TOOL_VERSION}"
   else
     url "https://github.com/kogai/golip/releases/download/v#{TOOL_VERSION}/golip_#{TOOL_VERSION}_darwin_386.zip"
-    sha1 "0d22f77af72d40b1c79f53fae84df7bbae4f73c2"
+    sha256 "d0816d41357b9b47d165d3e97b2ccdb749ad6310c3179eb0ea6a123bb95a6b27"
     version "v#{TOOL_VERSION}"
   end
 
