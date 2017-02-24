8501f5fc7d35c8458830c1b3547efd3bb6203278
小飼慎一 <kogai0121@gmail.com>
2017-02-24 23:15:22 +0900

    update sha-256

--- golip.rb
+++ golip.rb
@@ -5,11 +5,11 @@ TOOL_VERSION = '0.1.3'
 class Golip < Formula
   if Hardware.is_64_bit?
     url "https://github.com/kogai/golip/releases/download/v#{TOOL_VERSION}/golip_#{TOOL_VERSION}_darwin_amd64.zip"
-    sha256 "d0816d41357b9b47d165d3e97b2ccdb749ad6310c3179eb0ea6a123bb95a6b27"
+    sha256 "fe31d7aef2ce2353a91ec8fbf6d80c2c602fd8f449dfa77ae345d579a3fe5ff6"
     version "v#{TOOL_VERSION}"
   else
     url "https://github.com/kogai/golip/releases/download/v#{TOOL_VERSION}/golip_#{TOOL_VERSION}_darwin_386.zip"
-    sha256 "d0816d41357b9b47d165d3e97b2ccdb749ad6310c3179eb0ea6a123bb95a6b27"
+    sha256 "fe31d7aef2ce2353a91ec8fbf6d80c2c602fd8f449dfa77ae345d579a3fe5ff6"
     version "v#{TOOL_VERSION}"
   end
 
